4e24128e2ab61204ecab936002662b9e0ea24ffa
Swerdloj <swerdloj@andrews.edu>
2020-03-02 15:05:15 -0500

    Updated lexer. Following Rust's design pattern.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,31 +1,64 @@
-enum Token {
-    identifier(String), // TODO: How to handle the type? Part of AST node?
-    keyword(String),    // TODO: is this correct?
-    
-    Assignment,         // "="
-    TypeSpecifier,      // ":"
-    StatementEnd,       // ";"
+// Modeled after the Rust lexer:
+// See https://github.com/rust-lang/rust/blob/master/src/librustc_lexer/src/lib.rs
 
-    ScopeBegin,         // "{"
-    ScopeEnd,           // "}"
-    ConstructorBegin,   // "{"
-    ConstructorEnd,     // "}"
-    FunctionBegin,      // "("
-    FunctionEnd,        // ")"
+/// All allowed literal values within the language
+enum Literal {
+    Int,                // Numbers without decimals
+    Float,              // Numbers with decimals
+    // Str,             // NOTE: sdf-lang does not allow strings, chracters, etc. -> Maybe in the future for debugging
+}
 
-    OperatorMethod,     // "."
-    OperatorField,      // "."    
-    OperatorPlus,       // "+"
-    OperatorMinus,      // "-"
-    OperatorDivide,     // "/"
-    OperatorMultiply,   // "*"
-
-    // TODO: How to handle comments?
+/// All allowed characters within the language. 
+/// 
+/// Note that this is not a language token, rather a string decomposition
+enum TokenKind {
     CommentSingle,      // "//"
-    CommentMultiBegin,  // "/*"
-    CommentMultiEnd,    // "*/"
+    CommentMulti,       // "/* .. */"
+    
+    Whitespace,         // Anything like space, tab, linefeed, etc.
+
+    Literal(Literal),   // Typed values
+    Identifier(String), // Includes keywords
+    
+    Equals,             // "="
+    Colon,              // ":"
+    Comma,              // ","
+    Semicolon,          // ";"
+    Dot,                // "."
+
+    ParenthesisOpen,    // "("
+    ParenthesisClose,   // ")"
+    BraceOpen,          // "{"
+    BraceClose,         // "}"
+    BracketOpen,        // "["
+    BracketClose,       // "]"
+
+    At,                 // "@"
+
+    Star,               // "*"
+    Plus,               // "+"
+    Minus,              // "-"
+    Slash,              // "/"
+
+    Not,                // "!"
+    And,                // "&"
+    Or,                 // "|"
+    Tilde,              // "~"
+    LessThan,           // "<"
+    GreaterThan,        // ">"
+    Caret,              // "^"
+    Percent,            // "%"
+
+    Unknown,            // Invalid character
 }
 
 pub fn tokenize_string(string: String) {
+    /*
+        TODO:
+        1. Generate TokenKinds
+        2. ?
+    */
+
+
     println!("TODO: Tokenize string");
 }
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ mod lexer;
 fn main() {
     let env = environment::Environment::get();
 
-    // Note that file's existence will be checked above
+    println!("Reading from {:?} ...", &env.input_path);
+
+    // Note that file's existence will be checked already
     let input = std::fs::read_to_string(&env.input_path).expect("Failed to read input file");
+
+
     lexer::tokenize_string(input);
 
     println!("{:?}", env);
